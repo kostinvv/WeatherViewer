47550c03e33028096bc30aa11a7a902e99beceec
Vladislav Kostin <vladislavkostin.ds@yandex.ru>
2023-12-17 18:10:00 +0300

    Сreate a login and registration form

---
+++ WeatherViewer/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherViewer.Controllers;
+
+public class UserController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Registration()
+    {
+        return View();
+    }
+}
