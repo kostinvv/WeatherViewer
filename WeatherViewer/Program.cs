e2263fe95c96397bea96ab2f1dc94c703a171137
Vladislav Kostin <vladislavkostin.ds@yandex.ru>
2023-12-19 19:44:30 +0300

    Add session support

--- WeatherViewer/Program.cs
+++ WeatherViewer/Program.cs
@@ -5,12 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.IsEssential = true;
+});
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
+// add middleware
 app.UseStaticFiles();
+app.UseAuthorization();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
