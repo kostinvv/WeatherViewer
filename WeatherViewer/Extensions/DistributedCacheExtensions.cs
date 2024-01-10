b092984443cfacffd01b180daee8b089ef90bac2
Vladislav Kostin <vladislavkostin.ds@yandex.ru>
2024-01-10 13:18:39 +0300

    Add extension methods to work with cache

---
+++ WeatherViewer/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace WeatherViewer.Extensions;
+
+public static class DistributedCacheExtensions
+{
+    public static async Task SetRecordAsync<T>(
+        this IDistributedCache cache, 
+        string key,
+        T data,
+        TimeSpan? absoluteExpireTime = null,
+        TimeSpan? unusedExpireTime = null)
+    {
+        var options = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromMinutes(1),
+            SlidingExpiration = unusedExpireTime
+        };
+
+        var jsonString = JsonSerializer.Serialize(data);
+        await cache.SetStringAsync(key, value: jsonString, options);
+    }
+
+    public static async Task<T?> GetRecordAsync<T>(
+        this IDistributedCache cache, 
+        string key)
+    {
+        var jsonString = await cache.GetStringAsync(key);
+        
+        return jsonString is null ? default(T) : JsonSerializer.Deserialize<T>(jsonString);
+    }
+}
