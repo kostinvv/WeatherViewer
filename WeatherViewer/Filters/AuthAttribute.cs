fb7aaf912da7f7df2d74d3a62ee8114bc178895c
Vladislav <vladislavkostin.ds@gmail.com>
2024-01-15 12:40:29 +0300

    Create AuthAttribute.cs

---
+++ WeatherViewer/Filters/AuthAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WeatherViewer.Filters;
+
+public class AuthAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!context.HttpContext.Request.Cookies.ContainsKey("SessionId"))
+        {
+            context.Result = new RedirectToRouteResult(
+                new RouteValueDictionary(new { controller = "user", action = "login" })
+            );
+        }
+    }
+}
